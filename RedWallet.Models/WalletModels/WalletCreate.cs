a81241a7f5d82d8e2332af4c92c17f01934cf883
Dean Collier <daccollier@gmail.com>
2021-11-14 22:46:15 -0500

    updated dashboard

--- RedWallet.Models/WalletModels/WalletCreate.cs
+++ RedWallet.Models/WalletModels/WalletCreate.cs
@@ -16,12 +16,12 @@ namespace RedWallet.Models.WalletModels
         public string WalletName { get; set; }
 
         [Required]
-        [Display(Name = "Craete Wallet Passphrase")]
+        [Display(Name = "Create Wallet Passphrase", Description = "*This passphrase is specific to this wallet and should be different from your user password*")]
         [MinLength(8), DataType(DataType.Password)]
         public string Passphrase { get; set; }
 
         [Required]
-        [MinLength(8), Display(Name = "Entropy Input", Description = "Enter random letters for enhanced security.")]
+        [MinLength(8), Display(Name = "Entropy Input", Description = "*Enter random characters to enhance security. You will not need to remember these.*")]
         public string EntropyInput { get; set; }
 
         public string UserId { get; set; }
